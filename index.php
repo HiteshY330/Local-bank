7d6d0fc99465e67020c96afb0ddd11be10299ed3
Hitesh <hiteshyaduvanshi46@gmail.com>
2021-11-20 13:00:15 +0530

    local bank

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+include 'top.php';
+?>
+<!-- Caraousel-->
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slide04.jpg" alt="Easy Transfer" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Easy Money Transfer</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide03.jpg" alt="Quick Deposit" width="1100" height="500">
+      <div class="carousel-caption  d-none d-md-block">
+    <h3>Quick Personal Loans</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide05.jpg" alt="Low interests" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Customer Satisfaction</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide02.jpg" alt="Low interests" width="1100" height="500">
+      <div class="carousel-caption">
+    <h3>Customer</h3>
+    </div>
+  </div>
+  
+ <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<!--About us-->
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center h1 " > <u> About Us </u></h3>
+ </div>
+<div class="container-fluid">
+ <div class ="row">
+<div  class="col-lg-6 col-md-6 col-12">
+      <img src="images/edu_loan.jpg" class="img-fluid"  width="1000" height="900">
+      </div>
+      <div  class="col-lg-6 col-md-6 col-12">
+      <h1>Local Bank</h1>
+      <p class="font-weight-bold h4">Local Co-Operative Bank Limited is a fast growing & strong bank with it’s headquarter in Mumbai. The Bank is focused on upgrading and improving the services by optimising the use of the latest technology as well as continuous training of its Management and staff. The bank was founded in Mumbai in 1968 & was renamed as the New India Co-Operative Bank Limited in 1977. Since then the bank has grown into a strong and scheduled multi state bank with a network of branches in various states offering a wide range of services including forex services, trade services, home loans, small business loans and more.
+
+      </p>
+        </div>  
+         
+ </div>
+ </div>
+
+ <!--Dsiclaimer-->
+</section>
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center h2" ><u>Disclaimer</u></h3>
+ <p1 class=" font-weight-bold text-align-center">This website is the
+      outcome of a intern project, and does not
+      necessarily represent the views of any organisation or any other individuals referenced or
+      acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the website, without explicit permission, provided that the content is accompanied by an acknowledgement that Zombie Bank website is the source and that it is clearly indicated if changes were made to the original content.
+  </p1>
+</div>
+
+<?php
+include 'footer.php';
+?>
