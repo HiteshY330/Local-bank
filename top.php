dfb3f42164ea362916a29f9e49be2c7438a5c5fd
Hitesh <hiteshyaduvanshi46@gmail.com>
2021-11-20 12:56:31 +0530

    basic banking system

---
+++ top.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Local Bank - Apaki khushiyo ki chabhi</title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- Navigation bar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Local Bank </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www1.oanda.com/currency/converter/">Currency Convertor </a>
+        </li>
+        
+</div>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
